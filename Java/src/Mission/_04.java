71f74dea04e9b9a232da21967c575a169dd0d1bb
Bong127 <ansqhdgus200@naver.com>
2024-08-12 23:54:49 +0900

    자바미션 5번까지

---
+++ Java/src/Mission/_04.java
@@ -0,0 +1,28 @@
+package Mission;
+
+import java.util.Scanner;
+
+public class _04 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("입력 : ");
+		int n  = sc.nextInt();
+		// 별의 개수
+		int star = 1;
+		
+		for (int i = 0; i <= n; i++) {
+			// 빈 공간 출력
+			for (int j = i; j < n; j++) {
+				System.out.print(" ");
+			}
+			// * 출력
+			for(int j = 0; j < star; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+			star += 2;
+		}
+		
+		sc.close();
+	}
+}
